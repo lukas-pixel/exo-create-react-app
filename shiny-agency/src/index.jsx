cbf9471ff479c0b2472fb2bca7b301b07ddfadb5
marine cherdo <marine.cherdo13@gmail.com>
2023-03-06 16:33:10 +0100

    correction version react-router

--- shiny-agency/src/index.jsx
+++ shiny-agency/src/index.jsx
@@ -1,10 +1,19 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import Home from './pages/Home/Home'
+import Survey from './pages/Survey'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
 
 ReactDOM.render(
   <React.StrictMode>
-    <Home />
+    <Router>
+      <Route exact path="/">
+        <Home />
+      </Route>
+      <Route path="/survey">
+        <Survey />
+      </Route>
+    </Router>
   </React.StrictMode>,
   document.getElementById('root')
 )
