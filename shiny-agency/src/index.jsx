f26a0a9bf36170c8d953cfe6d8cc8a1cbf25803c
marine cherdo <marine.cherdo13@gmail.com>
2023-03-07 10:57:09 +0100

    Créez une route pour les attraper toutes : 404

--- shiny-agency/src/index.jsx
+++ shiny-agency/src/index.jsx
@@ -1,21 +1,27 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { BrowserRouter as Router, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import Home from './pages/Home/Home'
 import Survey from './pages/Survey'
 import Header from './conponents/Header'
+import Error from './conponents/Error/index'
 
 
 ReactDOM.render(
   <React.StrictMode>
     <Router>
       <Header />
-      <Route exact path="/">
-        <Home />
-      </Route>
-      <Route path="/survey/:questionNumber">
-        <Survey />
-      </Route>
+      <Switch>
+        <Route exact path="/">
+         <Home />
+        </Route>
+        <Route path="/survey/:questionNumber">
+          <Survey />
+        </Route>
+        <Route>
+          <Error />
+        </Route>
+      </Switch>
     </Router>
   </React.StrictMode>,
   document.getElementById('root')
