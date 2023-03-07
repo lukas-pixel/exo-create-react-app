e69283ccb9696ed8b46a9d250c7271b0827a8f47
marine cherdo <marine.cherdo13@gmail.com>
2023-03-07 16:02:45 +0100

    Passez des props dans votre CSS

--- shiny-agency/src/conponents/Header/index.jsx
+++ shiny-agency/src/conponents/Header/index.jsx
@@ -6,13 +6,16 @@ const StyledLink = styled(Link)`
     color: #8186a0;
     text-decoration: none;
     font-size: 18px;
+    ${(props) => 
+        props.$isFullLink && 
+            `color: white; border-radius: 30px; background-color: #5843E4;`}
 `
 
 function Header() {
     return (
         <nav>
             <StyledLink to="/">Accueil</StyledLink>
-            <StyledLink to="/survey/1">Questionnaire</StyledLink>
+            <StyledLink to="/survey/1" $isFullLink>Questionnaire</StyledLink>
             <StyledLink to="/result">Results</StyledLink>
             <StyledLink to="/freelances">Freelances</StyledLink>
         </nav>
