6dcbfeea0f7b8f7e6f7df891e92dea5c8eaeedef
marine cherdo <marine.cherdo13@gmail.com>
2023-03-07 10:18:10 +0100

    Récupérez des paramètres dans vos URL

--- shiny-agency/src/index.jsx
+++ shiny-agency/src/index.jsx
@@ -1,16 +1,19 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
 import Home from './pages/Home/Home'
 import Survey from './pages/Survey'
-import { BrowserRouter as Router, Route } from 'react-router-dom'
+import Header from './conponents/Header'
+
 
 ReactDOM.render(
   <React.StrictMode>
     <Router>
+      <Header />
       <Route exact path="/">
         <Home />
       </Route>
-      <Route path="/survey">
+      <Route path="/survey/:questionNumber">
         <Survey />
       </Route>
     </Router>
