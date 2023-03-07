264a45e5b959f1ee789d875d2eebad21179a9884
marine cherdo <marine.cherdo13@gmail.com>
2023-03-07 14:53:59 +0100

    Installez PropTypes

--- shiny-agency/src/pages/Freelances/index.jsx
+++ shiny-agency/src/pages/Freelances/index.jsx
@@ -1,7 +1,36 @@
+import DefaultPicture from '../../assets/profile.png'
+import Card from '../../conponents/Card'
+
+const freelanceProfiles = [
+    {
+        name:'Jane Doe',
+        jobTitle: 'Devops',
+        picture: DefaultPicture,
+    },
+    {
+        name:'John Doe',
+        jobTitle: 'Developpeur frontend',
+        picture: DefaultPicture,
+    },
+    {
+        name:'Jeanne Biche',
+        jobTitle: 'Développeuse Fullstack',
+        picture: DefaultPicture,
+    },
+]
+
 function Freelances() {
     return (
         <div>
-            <h1>Freelance</h1>
+            <h1>Freelances 💻</h1>
+            {freelanceProfiles.map((profile, index) => (
+                <Card
+                    key={`${profile.name}-${index}`}
+                    label={profile.jobTitle}
+                    picture={profile.picture}
+                    title={42}
+                />
+            ))}
         </div>
     )
 }
